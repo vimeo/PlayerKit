0bd9a8f276acc39a778a6187a82c82b4fee2adf4
Gavin King <gavin@vimeo.com>
2017-03-08 11:11:50 -0500

    removing pods

--- Example/Pods/Target Support Files/PlayerKit/PlayerKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PlayerKit : NSObject
-@end
-@implementation PodsDummy_PlayerKit
-@end
--- Example/Pods/Target Support Files/Pods-PlayerKit_Tests/Pods-PlayerKit_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_PlayerKit_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PlayerKit_TestsVersionString[];
-
