c25652589ab3393128758f16a6811d36d193710a
Bryant Balatbat <bryant@vimeo.com>
2019-08-19 11:16:32 -0400

    Add documentation for PlayerView

--- Sources/PlayerView.swift
+++ Sources/PlayerView.swift
@@ -5,6 +5,10 @@
 //  Created by Balatbat, Bryant on 12/20/17.
 //
 
+/// This file creates a `typealias` called `PlayerView` that determines which core view component to use depending on
+/// which platform the binary is being compiled for. If iOS or tvOS, `UIKit` will be imported and `UIView` will be used.
+/// otherwise, if macOS is used, `AppKit` will be imported and `NSView` will be used.
+
 #if os(iOS) || os(tvOS)
     import UIKit
 #else
